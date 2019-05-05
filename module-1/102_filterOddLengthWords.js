33712523ab9057c6aac7f95eb1d18bbe8f11bc32
Mohammed <ayazm.140895@gmail.com>
2019-05-05 18:05:35 +0530

    add more files

---
+++ module-1/102_filterOddLengthWords.js
@@ -0,0 +1,18 @@
+/*
+Write a function called "filterOddLengthWords".
+
+Given an array of string, "filterOddLengthWords" returns an array containing only the elements of the given array whose lengths are odd numbers.
+
+var output = filterOddLengthWords(['there', 'it', 'is', 'now']);
+console.log(output); // --> ['there', "now']
+*/
+
+function filterOddLengthWords(words) {
+  return  words.filter(function(word){
+    if (word.length % 2 === 1){
+      return word;
+    }
+  });
+
+}
+
